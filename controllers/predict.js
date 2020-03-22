81c25e6777aab913685db36ae1575ce4e033e00b
sstehniy <sstehniy@gmail.com>
2020-03-22 12:48:53 +0200

    dagsd

--- controllers/predict.js
+++ controllers/predict.js
@@ -67,7 +67,7 @@ router.post('/file', upload.single('img'), async (req, res, next) => {
       imagePath
     );
     
-    await fs.unlink(`./public/images/${req.file.filename}`, ()=>{console.log('file deleted')})
+    fs.unlinkSync(`./public/images/${req.file.filename}`, ()=>{console.log('file deleted')})
 
     res.status(200).send(response);
   } catch (err) {
